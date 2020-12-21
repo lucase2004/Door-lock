bd46eb1e788af50bd43b8fa5334b495b8bd61c90
Lucas Echeverria <Lucase2004@gmail.com>
2020-12-20 17:27:07 -0800

    Fixed Receiver file spelling and added LED functions as well as comments

--- TransmitterCode/TransmitterCode.ino
+++ TransmitterCode/TransmitterCode.ino
@@ -4,34 +4,43 @@
 #include <RF24.h>
 
 RF24 radio(7, 8); // CE, CSN
-const byte address[6] = "00001";
-bool power;
+
+const byte address[6] = "00001"; //sets address to a constant byte
+const int ledPin = 4; //const won't change
+const int buttonPin = 2;
+
+int buttonState = 0;  //variable for reading the pushbutton status
+bool power; //?
 
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT);
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+  pinMode(buttonPin, INPUT); //set buttonpin as input
+  pinMode(ledPin, OUTPUT); //set LED pin to output
+  radio.begin(); //begins NRF 
+  radio.openWritingPipe(address); //opens ability to send data
+  radio.setPALevel(RF24_PA_MIN); //?
+  radio.stopListening(); //transmitter doesnt need to listen (needs to be changed for new communciation method)
 }
 void loop() {
-  int val = digitalRead(2);
-  if (val == HIGH){
-    if (power) {
-      power = false;
-      Serial.println("Off");
+  buttonState = digitalRead(buttonPin); //reads button value 
+  if (buttonState == HIGH){ // check if button is pressed
+    if (power) { //?
+      power = false; //??
+      Serial.println("Unlocked");
+      digitalWrite(ledPin, HIGH); //turn LED on
       boolean state = 0;
       radio.write(&state, sizeof(state));
       delay(1000);
+      digitalWrite(ledPin, LOW); // turn off LED after 1 sec
     }
     else {
-      //add LED command here to lightup for one second
+      digitalWrite(ledPin, HIGH); //turn LED on
       power = true;
-      Serial.println("On");
+      Serial.println("Locked");
       boolean state = 1;
-      radio.write(&state, sizeof(state));
+      radio.write(&state, sizeof(state)); //sends signal to receiver to control servo
       delay(1000);
+      digitalWrite(ledPin, LOW); //turn off LED after 1 sec
     }
   }
 }
